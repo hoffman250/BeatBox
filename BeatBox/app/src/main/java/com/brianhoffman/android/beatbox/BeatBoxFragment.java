f55e6d56bd33e5719cf13acb9161bbe564ce1daa
hoffman250 <hoffman250@gmail.com>
2017-11-11 16:49:09 -0800

    Chp. 20, added list_item_sound.xml to project and SoundHolder to BeatBoxFragment.java.

--- BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
@@ -1,10 +1,16 @@
 package com.brianhoffman.android.beatbox;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.brianhoffman.android.beatbox.databinding.FragmentBeatBoxBinding;
+import com.brianhoffman.android.beatbox.databinding.ListItemSoundBinding;
 
 /**
  * Created by brianhoffman on 11/11/17.
@@ -21,6 +27,17 @@ public class BeatBoxFragment extends Fragment {
         FragmentBeatBoxBinding binding = DataBindingUtil
                 .inflate(inflater, R.layout.fragment_beat_box, container, false);
 
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+
         return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
     }
 }
