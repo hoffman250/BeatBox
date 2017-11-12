25280e9a86a363e0900a4e2a4ea30bb6bd83d178
hoffman250 <hoffman250@gmail.com>
2017-11-11 16:38:02 -0800

    Chp. 20, Added BeatBox binding code.

--- BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
@@ -1,6 +1,10 @@
 package com.brianhoffman.android.beatbox;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by brianhoffman on 11/11/17.
@@ -9,5 +13,14 @@ import android.support.v4.app.Fragment;
 public class BeatBoxFragment extends Fragment {
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        return binding.getRoot();
     }
 }
