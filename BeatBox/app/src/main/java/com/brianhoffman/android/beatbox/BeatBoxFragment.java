93fd6c83e01f098d7a185a7e54ad393f8713d4ce
hoffman250 <hoffman250@gmail.com>
2017-11-11 16:20:16 -0800

    Chp. 20, added BeatBoxFragment.java, SingleFragmentActivity.java, and SingleFragmentActivity.xml .

---
+++ BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
@@ -0,0 +1,13 @@
+package com.brianhoffman.android.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by brianhoffman on 11/11/17.
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
