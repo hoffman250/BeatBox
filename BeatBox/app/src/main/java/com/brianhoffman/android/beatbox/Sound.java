6fb87dd8d42498e4b926793265bbbd501e6c494b
hoffman250 <hoffman250@gmail.com>
2017-11-11 19:38:23 -0800

    Chp. 20, added Sound.java.

---
+++ BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.brianhoffman.android.beatbox;
+
+/**
+ * Created by brianhoffman on 11/11/17.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+    
+    public String getName() {
+        return mName;
+    }
+}
