24ddf96f0b4b485ba849ed20cb162a39c4726cd5
hoffman250 <hoffman250@gmail.com>
2017-11-11 18:56:55 -0800

    Chp. 20, Added sound files, created BeatBox class.

--- BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/brianhoffman/android/beatbox/BeatBoxFragment.java
@@ -17,8 +17,18 @@ import com.brianhoffman.android.beatbox.databinding.ListItemSoundBinding;
  */
 
 public class BeatBoxFragment extends Fragment {
+
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
@@ -28,6 +38,7 @@ public class BeatBoxFragment extends Fragment {
                 .inflate(inflater, R.layout.fragment_beat_box, container, false);
 
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
 
         return binding.getRoot();
     }
@@ -38,6 +49,26 @@ public class BeatBoxFragment extends Fragment {
         private SoundHolder(ListItemSoundBinding binding) {
             super(binding.getRoot());
             mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil
+                    .inflate(inflater, R.layout.list_item_sound, parent, false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
         }
     }
 }
